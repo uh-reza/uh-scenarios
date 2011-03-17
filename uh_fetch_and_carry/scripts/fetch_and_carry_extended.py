c9e181bbf333a91dc0c7a65a9749a1529acc37b9
uh-reza <oskoei@yahoo.com>
2011-03-17 10:46:13 +0000

    added fetch and carry extended script

---
+++ uh_fetch_and_carry/scripts/fetch_and_carry_extended.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+
+import time
+
+import roslib
+roslib.load_manifest('uh_fetch_and_carry')
+import rospy
+
+from simple_script_server import script
+
+import tf
+from geometry_msgs.msg import *
+
+class FetchAndCarry(script):
+		
+	def Initialize(self):
+		# initialize components (not needed for simulation)
+		self.sss.init("tray")
+		self.sss.init("torso")
+		self.sss.init("arm")
+		self.sss.init("sdh")
+		self.sss.init("base")
+		
+		# move to initial positions
+		handle_arm = self.sss.move("arm","folded",False)
+		handle_torso = self.sss.move("torso","home",False)
+		handle_sdh = self.sss.move("sdh","home",False)
+		self.sss.move("tray","down")
+		handle_arm.wait()
+		handle_torso.wait()
+		handle_sdh.wait()
+		if not self.sss.parse:
+			print "Please localize the robot with rviz"
+		self.sss.wait_for_input()
+		
+	def Run(self): 
+		listener = tf.TransformListener(True, rospy.Duration(10.0))
+	
+		# going to sofa offering drink
+		handle_base = self.sss.move("base","tv",False)
+		while not (handle_base.get_state() == 3)  
+			self.sss.set_light("red")
+			sleep(.5)
+			self.sss.set_light([0,0,0])
+			sleep(.5)
+		handle_base.wait()
+		self.sss.set_light("green")
+		
+		self.sss.say(["Hello there, can I get you anything to dirink"], False)
+		self.sss.move("torso","nod")
+		if not self.sss.parse:
+			print "Please have your choice"
+		choice = self.sss.wait_for_input()
+
+		# caculate tranformations, we need cup coordinates in arm_7_link coordinate system
+		cup = PointStamped()
+		cup.header.stamp = rospy.Time.now()
+		cup.header.frame_id = "/map"
+		if choice == "1"
+			cup.point.x = -1.6
+			cup.point.y = 1.0
+			cup.point.z = 0.86
+		if choice == "2"
+			cup.point.x = -1.6
+			cup.point.y = 1.0
+			cup.point.z = 0.86
+		if choice == "3"
+			cup.point.x = -1.6
+			cup.point.y = 1.0
+			cup.point.z = 0.86
+		self.sss.sleep(2) # wait for transform to be calculated
+
+		handle_base = self.sss.move("base","shelf",False)
+		blink(handle_base,"red")
+		handle_arm = self.sss.move("arm","pregrasp")
+		blink(handle_arm,"yellow")
+		handle_sdh = self.sss.move("sdh","cylopen")
+		blink(handle_arm,"yellow")
+
+		if not self.sss.parse:
+			cup = listener.transformPoint('/arm_7_link',cup)
+			# transform grasp point to sdh center
+			cup.point.z = cup.point.z + 0.2
+
+		# move in front of cup
+		pregrasp_distance = 0.2
+		self.sss.move_cart_rel("arm",[[cup.point.x, cup.point.y, cup.point.z+pregrasp_distance], [0, 0, 0]])
+
+		# move to cup
+		self.sss.move_cart_rel("arm",[[0.0, 0.0, -pregrasp_distance], [0, 0, 0]])
+		# grasp cup
+		self.sss.move("sdh","cylclosed")
+		# lift cup
+		self.sss.move_cart_rel("arm",[[0.0, 0.1, 0.0], [0, 0, 0]])
+
+		# place cup on tray
+		handle01 = self.sss.move("arm","grasp-to-tray",False)
+		self.sss.move("tray","up")
+		handle01.wait()
+
+		self.sss.move("sdh","cylopen")
+
+		self.sss.move_cart_rel("arm",[[0.0, 0.0, 0.2], [0, 0, 0]])
+		handle01 = self.sss.move("arm","tray-to-folded",False)
+		self.sss.sleep(4)
+		self.sss.move("sdh","cylclosed",False)
+		handle01.wait()
+
+		# deliver cup to order position
+		self.sss.move("base","tv")
+		self.sss.say("Here's your drink.")
+		self.sss.move("torso","nod")
+
+	def blink(self, handle, color):
+		while not (handle.get_state() == 3)  
+			self.sss.set_light(color)
+			sleep(.5)
+			self.sss.set_light([0,0,0])
+			sleep(.5)
+		handle.wait()
+ 
+
+if __name__ == "__main__":
+	SCRIPT = FetchAndCarry()
+	SCRIPT.Start()
