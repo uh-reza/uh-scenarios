7f621aeadbcdec36231df3f2308043ad02dfc656
reza <reza@ubuntu.(none)>
2011-07-11 16:49:17 +0100

    11 jul 2011

---
+++ people/face_detector/build/CMakeFiles/ROSBUILD_genmsg_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+FILE(REMOVE_RECURSE
+  "../src/face_detector/msg"
+  "../msg_gen"
+  "../msg_gen"
+  "CMakeFiles/ROSBUILD_genmsg_cpp"
+  "../msg_gen/cpp/include/face_detector/FaceDetectorAction.h"
+  "../msg_gen/cpp/include/face_detector/FaceDetectorGoal.h"
+  "../msg_gen/cpp/include/face_detector/FaceDetectorActionGoal.h"
+  "../msg_gen/cpp/include/face_detector/FaceDetectorResult.h"
+  "../msg_gen/cpp/include/face_detector/FaceDetectorActionResult.h"
+  "../msg_gen/cpp/include/face_detector/FaceDetectorFeedback.h"
+  "../msg_gen/cpp/include/face_detector/FaceDetectorActionFeedback.h"
+  "../msg/FaceDetectorAction.msg"
+  "../msg/FaceDetectorGoal.msg"
+  "../msg/FaceDetectorActionGoal.msg"
+  "../msg/FaceDetectorResult.msg"
+  "../msg/FaceDetectorActionResult.msg"
+  "../msg/FaceDetectorFeedback.msg"
+  "../msg/FaceDetectorActionFeedback.msg"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/ROSBUILD_genmsg_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ people/face_detector/build/CMakeFiles/face_detector.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/reza/git/people/face_detector/src/face_detection.cpp" "/home/reza/git/people/face_detector/build/CMakeFiles/face_detector.dir/src/face_detection.o"
+  "/home/reza/git/people/face_detector/src/faces.cpp" "/home/reza/git/people/face_detector/build/CMakeFiles/face_detector.dir/src/faces.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
